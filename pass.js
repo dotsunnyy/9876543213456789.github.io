41bc10e665d71d700dbeeb3c1e836d80f20edd67
eli <110707180+dotsunnyy@users.noreply.github.com>
2024-06-10 07:17:26 -0600

    er

--- pass.js
+++ pass.js
@@ -997,4 +997,4 @@ var password = "water";
       return passcodeprotect();
    }
 }());
-alert('good job\n\nchangelog 6/10:\nadded 2 new games.\n"smash ur computer", "papas freezeria"');
+alert('good job\n\nchangelog 6/10:\nadded 3 new games.\n"smash ur computer", "papas freezeria", "papas bakeria"');
